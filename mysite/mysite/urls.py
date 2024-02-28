83a90429f1710d933bfb4ef5dff50bb8b529dfa5
ebac349 <ebac349@gmail.com>
2024-02-28 02:19:59 -0300

    templates

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home', include('blog.urls'))
+    path('home', include('blog.urls')),
+    path('post_detail', include('blog.urls'))
 ]
